dc1ccdefcfc433c421bac90b696e225d788e863c
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2017-07-20 11:15:51 +0300

    +

--- html/html/apps/calendar/calendar.js
+++ html/html/apps/calendar/calendar.js
@@ -137,81 +137,81 @@
       $('#calendar').fullCalendar(_options);
     },
 
-    handleSelective: function() {
+    // handleSelective: function() {
 
-      var member = [{
-        id: 'uid_1',
-        name: 'Herman Beck',
-        avatar: '../../../assets/portraits/1.jpg'
-      }, {
-        id: 'uid_2',
-        name: 'Mary Adams',
-        avatar: '../../../assets/portraits/2.jpg'
-      }, {
-        id: 'uid_3',
-        name: 'Caleb Richards',
-        avatar: '../../../assets/portraits/3.jpg'
-      }, {
-        id: 'uid_4',
-        name: 'June Lane',
-        avatar: '../../../assets/portraits/4.jpg'
-      }];
+    //   var member = [{
+    //     id: 'uid_1',
+    //     name: 'Herman Beck',
+    //     avatar: '../../../assets/portraits/1.jpg'
+    //   }, {
+    //     id: 'uid_2',
+    //     name: 'Mary Adams',
+    //     avatar: '../../../assets/portraits/2.jpg'
+    //   }, {
+    //     id: 'uid_3',
+    //     name: 'Caleb Richards',
+    //     avatar: '../../../assets/portraits/3.jpg'
+    //   }, {
+    //     id: 'uid_4',
+    //     name: 'June Lane',
+    //     avatar: '../../../assets/portraits/4.jpg'
+    //   }];
 
-      var items = [{
-        id: 'uid_1',
-        name: 'Herman Beck',
-        avatar: '../../../assets/portraits/1.jpg'
-      }, {
-        id: 'uid_2',
-        name: 'Caleb Richards',
-        avatar: '../../../assets/portraits/2.jpg'
-      }];
+    //   var items = [{
+    //     id: 'uid_1',
+    //     name: 'Herman Beck',
+    //     avatar: '../../../assets/portraits/1.jpg'
+    //   }, {
+    //     id: 'uid_2',
+    //     name: 'Caleb Richards',
+    //     avatar: '../../../assets/portraits/2.jpg'
+    //   }];
       
 
-      $('[data-plugin="jquery-selective"]').selective({
-        namespace: 'addMember',
-        local: member,
-        selected: items,
-        buildFromHtml: false,
-        tpl: {
-          optionValue: function(data) {
-            return data.id;
-          },
-          frame: function() {
-            return '<div class="' + this.namespace + '">' +
-              this.options.tpl.items.call(this) +
-              '<div class="' + this.namespace + '-trigger">' +
-              this.options.tpl.triggerButton.call(this) +
-              '<div class="' + this.namespace + '-trigger-dropdown">' +
-              this.options.tpl.list.call(this) +
-              '</div>' +
-              '</div>' +
-              '</div>'
-          },
-          triggerButton: function() {
-            return '<div class="' + this.namespace + '-trigger-button"><i class="wb-plus"></i></div>';
-          },
-          listItem: function(data) {
-            return '<li class="' + this.namespace + '-list-item"><img class="avatar" src="' + data.avatar + '">' + data.name + '</li>';
-          },
-          item: function(data) {
-            return '<li class="' + this.namespace + '-item"><img class="avatar" src="' + data.avatar + '" title="' + data.name + '">' +
-              this.options.tpl.itemRemove.call(this) +
-              '</li>';
-          },
-          itemRemove: function() {
-            return '<span class="' + this.namespace + '-remove"><i class="wb-minus-circle"></i></span>';
-          },
-          option: function(data) {
-            return '<option value="' + this.options.tpl.optionValue.call(this, data) + '">' + data.name + '</option>';
-          }
-        }
-      });
-    },
+    //   $('[data-plugin="jquery-selective"]').selective({
+    //     namespace: 'addMember',
+    //     local: member,
+    //     selected: items,
+    //     buildFromHtml: false,
+    //     tpl: {
+    //       optionValue: function(data) {
+    //         return data.id;
+    //       },
+    //       frame: function() {
+    //         return '<div class="' + this.namespace + '">' +
+    //           this.options.tpl.items.call(this) +
+    //           '<div class="' + this.namespace + '-trigger">' +
+    //           this.options.tpl.triggerButton.call(this) +
+    //           '<div class="' + this.namespace + '-trigger-dropdown">' +
+    //           this.options.tpl.list.call(this) +
+    //           '</div>' +
+    //           '</div>' +
+    //           '</div>'
+    //       },
+    //       triggerButton: function() {
+    //         return '<div class="' + this.namespace + '-trigger-button"><i class="wb-plus"></i></div>';
+    //       },
+    //       listItem: function(data) {
+    //         return '<li class="' + this.namespace + '-list-item"><img class="avatar" src="' + data.avatar + '">' + data.name + '</li>';
+    //       },
+    //       item: function(data) {
+    //         return '<li class="' + this.namespace + '-item"><img class="avatar" src="' + data.avatar + '" title="' + data.name + '">' +
+    //           this.options.tpl.itemRemove.call(this) +
+    //           '</li>';
+    //       },
+    //       itemRemove: function() {
+    //         return '<span class="' + this.namespace + '-remove"><i class="wb-minus-circle"></i></span>';
+    //       },
+    //       option: function(data) {
+    //         return '<option value="' + this.options.tpl.optionValue.call(this, data) + '">' + data.name + '</option>';
+    //       }
+    //     }
+    //   });
+    // },
 
-    handleAction: function() {
-      var actionBtn = $('.site-action').actionBtn().data('actionBtn');
-    },
+    // handleAction: function() {
+    //   var actionBtn = $('.site-action').actionBtn().data('actionBtn');
+    // },
 
     handleEventList: function() {
       $('#addNewEventBtn').on('click', function() {
